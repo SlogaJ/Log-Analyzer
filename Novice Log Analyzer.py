c58d07f1805b0b872358f4c14e85a5da5fe286a0
SlogaJ <jerry_hrndz@icloud.com>
2025-05-28 22:29:21 -0500

    defined multiple input functions and added a call at the end

--- Novice Log Analyzer.py
+++ Novice Log Analyzer.py
@@ -3,42 +3,44 @@
 valid_status_codes = ["200", "301", "302", "400", "401", "403", "404", "500", "502", "503"]
 valid_http_methods = ["GET", "POST"]
 
+
+def user_input():
 #prompt the user to enter their log file path they want to analyze
-log_path = input("Enter the log file path here: ").strip()
+    log_path = input("Enter the log file path here: ").strip()
+    target_ip = input("Type in the IP address you wish to filter for (or press Enter to skip): ").strip()
+    target_status_code = input(f"Please enter the Status Code you wish to filter by {valid_status_codes} (or press Enter to skip): ").strip( )
+    target_http_method = input(f"Please enter the HTTP method you wish to filter by {valid_http_methods} (or press Enter to skip): ").upper().strip()
+
+    return log_path, target_ip, target_status_code, target_http_method
 
 
-#have the user enter what they're searching for or if they want to skip
-
-target_ip = input("Type in the IP address you wish to filter for (or press Enter to skip): ").strip()
-target_status_code = input(f"Please enter the Status Code you wish to filter by {valid_status_codes} (or press Enter to skip): ").strip( )
-target_http_method = input(f"Please enter the HTTP method you wish to filter by {valid_http_methods} (or press Enter to skip): ").upper().strip()
-
+def input_validation(status_code, http_method):
 #compares user input to valid inputs and if matched, opens the log provided and searches for matches
 
-if target_status_code and target_status_code not in valid_status_codes:
-    print(f"Invalid Status Code entry. Please enter one of the valid options {valid_status_codes}")
-    exit()
+    if status_code and status_code not in valid_status_codes:
+        print(f"Invalid Status Code entry. Please enter one of the valid options {valid_status_codes}")
+        exit()
 
-if target_http_method and target_http_method not in valid_http_methods:
-    print(f"Invalid HTTP Method entry. Please enter of of the valid options {valid_http_methods}")
-    exit()
+    if http_method and http_method not in valid_http_methods:
+        print(f"Invalid HTTP Method entry. Please enter of of the valid options {valid_http_methods}")
+        exit()
+
+def analyze_log_file(log_path, ip_filter, code_filter, method_filter):
+    try:
+        with open(log_path, "r") as file:
+            print("\n Matching log entries:\n" + "-" * 40)
+            found = False
+            for line in file:
+                cleaned_line = line.strip()
+                if not cleaned_line:
+                    continue
 
 
-try:
-    with open(log_path, "r") as file:
-        print("\n Matching log entries:\n" + "-" * 40)
-        found = False
-        for line in file:
-            cleaned_line = line.strip()
-            if not cleaned_line:
-                continue
-
-
-            log_part = cleaned_line.split(" ")
-            if len(log_part) >= 9:
+                log_part = cleaned_line.split(" ")
+                if len(log_part) >= 9:
                     ip_address = log_part[0]
                     timestamp = log_part[3].strip("[]")
-                    request_method = log_part[4].strip('"')
+                    http_method = log_part[4].strip('"')
                     resource = log_part[5]
                     protocol = log_part[6].strip('"')
                     status_code = log_part[7]
@@ -46,16 +48,25 @@ try:
 
 #once matched it will print out results unless entry was skipped
 
-            if (
-                    (not target_ip or ip_address == target_ip) and
-                    (not target_status_code or status_code == target_status_code) and
-                    (not target_http_method or request_method == target_http_method)
+                if (
+                    (not ip_filter or ip_address == ip_filter) and
+                    (not code_filter or status_code == code_filter) and
+                    (not method_filter or http_method == method_filter)
                 ):
                     print(cleaned_line)
                     found = True
-        if not found:
-            print("No matching entries found.")
-except FileNotFoundError:
-    print(f"Error: Log file not found in path provided: {log_path}")
-except Exception as e:
-    print(f"An error occured: {e}")
+            if not found:
+                print("No matching entries found.")
+    except FileNotFoundError:
+        print(f"Error: Log file not found in path provided: {log_path}")
+    except Exception as e:
+        print(f"An error occured: {e}")
+
+def main():
+    log_path, ip, code, method = user_input()
+    input_validation(code, method)
+    analyze_log_file(log_path, ip, code, method)
+
+if __name__ == "__main__":
+    main()
+
